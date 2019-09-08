def65351c6092f49c921cf03ecd486b914e64dce
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-08 21:09:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name_array)
+  name_array.each_with_index { |item, index| puts "#{index+1}. #{item}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(name_array)
+  name_array.map {|item| p "#{item.capitalize!}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(name_array)
+  name_array.any? {|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(name_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese = name_array & (cheese_types).to_a 
+  cheese[0]
 end
